7d660f69a1031f0f22bf3305da4bcdfae486717a
somratsam <somratsam2@gmail.com>
2023-05-11 23:40:35 +0600

    create recipe json file

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const cors = require("cors")
 const app = express();
 app.use(cors());
 const allChefs = require("./data.json")
-
+const allRecipes = require("./recipe.json")
 app.get('/', (req, res) => {
     res.json({massage: "hello server running"})
 })
 app.get ("/allData", (req, res) => {
     res.send(allChefs)
+})
+app.get ("/featuredRecipes", (req, res) => {
+    res.send(allRecipes)
+})
+app.get ("/chefTipsAndTricks", (req, res) => {
+    res.send(allRecipes)
 })
 
 
